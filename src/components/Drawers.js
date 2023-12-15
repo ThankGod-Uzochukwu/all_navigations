25d88276fae333ef8fe32740fb132f1b87de937f
TeeGee-TechBro <allin1acct.com@gmail.co>
2023-12-15 11:40:27 +0100

    got a persisting error that says "failed to create a worklet". unable to resolve this error.

--- src/components/Drawers.js
+++ src/components/Drawers.js
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Register = () => {
+const Drawers = () => {
   return (
     <View>
-      <Text>Register</Text>
+      <Text>Drawers</Text>
     </View>
   )
 }
 
-export default Register
+export default Drawers
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import AppNavigator from './srceens';
+
+
+
+
+const AllNav = () => {
+    return (
+        // <SafeAreaProvider>
+        //     <Provider store={store}>
+                <AppNavigator />
+        //     </Provider>
+        // </SafeAreaProvider>
+    );
+}
+
+export default AllNav
